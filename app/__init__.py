667e2f6f15dcc8f27d3407efae5bb4d06afaf2bc
araalifarooq <farooqp35h@gmail.com>
2018-09-28 22:30:56 +0300

    Add user authentication

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from app import views
+
+app.config['JWT_SECRET_KEY'] = 'sec-def-oscar-zulu-3-zero-niner'
+from flask_jwt_extended import JWTManager
+jwt = JWTManager(app)
